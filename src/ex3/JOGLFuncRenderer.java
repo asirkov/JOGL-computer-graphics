aa73d583c4c94a8e78421dd0b3adff2c14f9425a
asirkov <51347504+asirkov@users.noreply.github.com>
2019-11-03 04:03:34 +0200

    update drawAxis and drawFunction

--- src/ex3/JOGLFuncRenderer.java
+++ src/ex3/JOGLFuncRenderer.java
@@ -16,8 +16,8 @@ public class JOGLFuncRenderer {
     // Scaling value, (1 for 1 pixel = 1 coordinate)
     private static final int SCALING = 10;
 
-    // Epsilon for draw iterations
-    private static final int EPS = 100;
+    // Epsilon for draw iterations in pixels
+    private static final int EPS = 20;
 
 
     // Default colors
@@ -26,11 +26,10 @@ public class JOGLFuncRenderer {
         static final float[] BLACK = {0.0f, 0.0f, 0.0f};
     }
 
-    // Interface for drawable function
-    // x * 2
-    private static interface DrawableFunction extends Function<Integer, Integer> {
+    //Interface for drawable function
+    private static interface DrawableFunction extends Function<Float, Float> {
         @Override
-        public Integer apply(Integer x);
+        public Float apply(Float x);
     }
 
     public static void main(String[] args) {
@@ -53,8 +52,6 @@ public class JOGLFuncRenderer {
     }
 
 
-
-
     private static class Renderer implements GLEventListener {
 
         @Override
@@ -69,7 +66,11 @@ public class JOGLFuncRenderer {
             gl.glLoadIdentity();
 
             // Now Ortho set to (1 pixel = 1 coordinate); 0, 0 = center;
-            gl.glOrtho((int)(-1 * WINDOW_WIDTH / 2), (int)(WINDOW_WIDTH / 2), (int)(-1 * WINDOW_HEIGHT / 2), (int)(WINDOW_HEIGHT / 2), 1.0, -1.0);
+            gl.glOrtho((-1 * SCALING),
+                    (SCALING),
+                    (-1 * SCALING),
+                    (SCALING),
+                    1.0, -1.0);
         }
 
         @Override
@@ -79,114 +80,94 @@ public class JOGLFuncRenderer {
 
             drawAxis(gl, drawable);
 
-            //final DrawableFunction myFunc = x -> x * 2;
-            //drawFunction(gl, myFunc);
+            final DrawableFunction myFunc = x -> { return (float) Math.sin(x) + (5f * (float) Math.cos(x) ); };
+            drawFunction(gl, myFunc);
         }
 
-        /*
-         *
-         *
-         */
         private static void drawAxis(GL2 gl, GLAutoDrawable drawable) {
             final float[] CURRENT_COLOR = Color.BLACK;
+            final int TEXT_SIZE = 11;
+
             final int centerX = drawable.getSurfaceWidth() / 2;
             final int centerY = drawable.getSurfaceHeight() / 2;
-            final int TEXT_SIZE = 11;
 
             gl.glLineWidth(2f);
 
             // draw Y
             gl.glBegin(GL2.GL_LINES);
-                gl.glColor3fv(CURRENT_COLOR, 0);
+                //gl.glColor3fv(CURRENT_COLOR, 0);
+                gl.glColor3f(0f, 0f, 1f);
                 gl.glVertex2i(0, centerY);
                 gl.glVertex2i(0, -1 * centerY);
             gl.glEnd();
 
             // draw X
             gl.glBegin(GL2.GL_LINES);
-                gl.glColor3fv(CURRENT_COLOR, 0);
+                //gl.glColor3fv(CURRENT_COLOR, 0);
+                gl.glColor3f(0f, 0f, 1f);
                 gl.glVertex2i(centerX, 0);
                 gl.glVertex2i(-1 * centerX, 0);
             gl.glEnd();
 
+
             TextRenderer tr = new TextRenderer(new Font("Sans serif", Font.BOLD, TEXT_SIZE));
 
+            int deltaX = drawable.getSurfaceWidth() / 2 / SCALING;
+            int deltaY = drawable.getSurfaceHeight() / 2 / SCALING;
+
             tr.beginRendering(drawable.getSurfaceWidth(), drawable.getSurfaceHeight());
+                tr.setColor(CURRENT_COLOR[0], CURRENT_COLOR[1], CURRENT_COLOR[2], 1f);
+
                 tr.setColor(CURRENT_COLOR[0], CURRENT_COLOR[1], CURRENT_COLOR[2], 1f);
                 tr.draw("X", drawable.getSurfaceWidth() - TEXT_SIZE, centerY - TEXT_SIZE);
                 tr.draw("Y", centerX - TEXT_SIZE, drawable.getSurfaceHeight() - TEXT_SIZE);
-            tr.endRendering();
 
-            final int deltaX = centerX / SCALING;
-            final int deltaY = centerY / SCALING;
+                tr.draw("0", centerX - TEXT_SIZE, centerY - TEXT_SIZE);
 
-            final int offset = TEXT_SIZE * 2;
-            // add second offset
-
-            tr.beginRendering(drawable.getSurfaceWidth(), drawable.getSurfaceHeight());
-                tr.setColor(CURRENT_COLOR[0], CURRENT_COLOR[1], CURRENT_COLOR[2], 1f);
-
-                tr.draw("0", centerX - offset, centerY - offset);
                 for (int i = 1; i < SCALING; i++) {
+                    // Y \ -Y
+                    tr.draw(String.valueOf(i)       , centerX - TEXT_SIZE, centerY + (i * deltaY));
+                    tr.draw(String.valueOf(-1 * i)  , centerX - TEXT_SIZE, centerY + (-1 * i * deltaY));
 
-                    // Y
-                    tr.draw(String.valueOf(i), centerX - offset, centerY + (i * deltaY) - offset);
-                    // -Y
-                    tr.draw(String.valueOf(-1 * i), centerX - offset, centerY + (-1 * i * deltaY) - offset);
-
-                    // X
-                    tr.draw(String.valueOf(i), centerX + (i * deltaX) - offset, centerY - offset);
-                    // -X
-                    tr.draw(String.valueOf(-1 * i), centerX + (-1 * i * deltaX) - offset , centerY - offset);
+                    // X \ -X
+                    tr.draw(String.valueOf(i)       , centerX + (i * deltaX), centerY - TEXT_SIZE);
+                    tr.draw(String.valueOf(-1 * i)  , centerX + (-1 * i * deltaX), centerY - TEXT_SIZE);
                 }
             tr.endRendering();
 
+
+
             gl.glPointSize(4f);
+            gl.glEnable(GL2.GL_POINT_SMOOTH);
+
             gl.glBegin(GL2.GL_POINTS);
             gl.glColor3fv(CURRENT_COLOR, 0);
                 for (int i = 0; i < SCALING; i++) {
-                    gl.glVertex2i(0, (-1 * i * deltaY));
-                    gl.glVertex2i(0, i * deltaY);
-                    gl.glVertex2i(i * deltaX, 0);
-                    gl.glVertex2i((-1 * i * deltaX), 0);
+                    gl.glVertex2i(0, (-1 * i));
+                    gl.glVertex2i(0, i);
+                    gl.glVertex2i(i, 0);
+                    gl.glVertex2i((-1 * i), 0);
                 }
             gl.glEnd();
+            gl.glDisable(GL2.GL_POINT_SMOOTH);
         }
 
         private void drawFunction(GL2 gl, DrawableFunction f) {
-            gl.glEnable(GL2.GL_LINE_SMOOTH);
-
             final int A = 0;
-            final int B = 100;
-            final float delta = (float)(B - A) / EPS;
+            final int B = 8;
 
-            gl.glBegin(GL2.GL_LINES);
-                gl.glColor3f(1f, 0f,0f);
+            //gl.glLineWidth(1f);
+            float deltaX = Math.abs(B - A) / 40f;
 
-                for(int i = 0; i < EPS; i++) {
-                    gl.glVertex2i((int)(i * delta * SCALING), f.apply(i) * SCALING);
-                    gl.glVertex2i((int)(i * delta * SCALING) + (int)delta, f.apply(i) * SCALING + (int)delta);
+            gl.glBegin(GL2.GL_LINE_STRIP);
+                gl.glColor3f(1f, 0f, 0f);
+
+                for(float i = A; i <= B; i+= deltaX) {
+                    gl.glVertex2f(i, f.apply(i) );
                 }
             gl.glEnd();
 
-            gl.glDisable(GL2.GL_LINE_SMOOTH);
         }
-
-
-        // not used
-        private static class point2i {
-            int x;
-            int y;
-            point2i(int x, int y) {this.x = x; this.y = y;}
-        }
-
-
-
-        // not used
-        private static point2i avg(point2i A, point2i B) {
-            return new point2i((A.x + B.x) / 2, (A.y + B.y) / 2);
-        }
-
 
 
         // Not used
